d13e7e7010a51a5a331280d85c1b749673bc87df
William R Friend <william.friend777@gmail.com>
2023-05-29 20:52:07 -0400

    stretching laravel muscles

--- chirper/app/Providers/EventServiceProvider.php
+++ chirper/app/Providers/EventServiceProvider.php
@@ -6,7 +6,10 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-
+//mine
+use App\Events\ChirpCreated;
+use App\Listeners\SendChirpCreatedNotifications;
+//end mine
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -17,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        ChirpCreated::class => [
+            SendChirpCreatedNotifications::class,
         ],
     ];
 
--- srccode-jetstream/app/Models/User.php
+++ srccode-jetstream/app/Models/User.php
@@ -2,17 +2,21 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
-// mine
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -33,6 +37,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
@@ -42,17 +48,14 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 
     /**
-     * Get the chirps for the user.
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
      */
-    public function chirps(): HasMany
-
-    {
-
-        return $this->hasMany(Chirp::class);
-
-    }
+    protected $appends = [
+        'profile_photo_url',
+    ];
 }
